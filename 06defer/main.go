f0b5885c17ddab13f282d57732650d67ffe334d2
yang20002020 <2628636987@qq.com>
2022-12-21 00:33:16 +0800

    defer函数 读取文件内容关闭文件

---
+++ 06defer/main.go
@@ -0,0 +1,34 @@
+package  main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func readfile(fileName string){
+	//func Open(name string) (*File, error) {
+	//	return OpenFile(name, O_RDONLY, 0)
+	//}
+	//go语言一般会将错误信息作为最后一个参数返回
+
+	//err==nil 表示执行成功；err！=nil 表示有执行失败
+	f1,err:=os.Open(fileName)
+	defer f1.Close()
+	if err!=nil{
+		fmt.Println("打开文件失败！err:",err)
+
+	}
+	buf:=make([]byte,1024)
+	//func (f *File) Read(b []byte) (n int, err error)
+	n,_:=f1.Read(buf)
+	fmt.Println("读取文件的实际长度：",n)
+	fmt.Println("读取的文件内容：",string(buf))
+
+}
+
+func main(){
+	fileName:="D:\\golandProject\\src\\00import\\main.go"
+	readfile(fileName)
+}
+

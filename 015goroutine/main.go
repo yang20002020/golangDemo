73a8dfef83a6ecb104902e61a276a34f073d66e4
26286 <2628636987@qq.com>
2023-03-14 18:04:53 +0800

    goroutine

---
+++ 015goroutine/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 这个将用于子go程使用
+func display(num int) {
+	count := 1
+	for {
+		fmt.Println("==============》这是子go程：", num, "当前count值：", count)
+		count++
+		time.Sleep(1 * time.Second)
+	}
+}
+func main() {
+	//子go程
+	for i := 0; i < 3; i++ {
+		go display(i)
+	}
+
+	//主go程
+	count := 1
+	for {
+		fmt.Println("这是主go程：", count)
+		count++
+		time.Sleep(1 * time.Second)
+	}
+
+}
